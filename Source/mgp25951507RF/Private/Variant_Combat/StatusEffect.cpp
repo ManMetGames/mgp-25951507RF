f37fe41fbd4e9b15e6ad03bcea5ff376d24bb499
25951507RF <25951507@stu.mmu.ac.uk>
2026-04-28 15:09:30 +0100

    Added StatusEffect (Twice)
    Accidentally created two files for the Status Effect code, will be using the one without my Username as the primary.

---
+++ Source/mgp25951507RF/Private/Variant_Combat/StatusEffect.cpp
@@ -0,0 +1,14 @@
+#include "Variant_Combat/StatusEffect.h"
+
+/* The Plan: implement status effect functionality.
+* Add Status Effect Types (Fire, Electric, Toxic, Acid, etc.)
+* Create a system for applying status effects to characters, including duration, DoT (Damage over Time).
+* Extra: Add an effect that causes the player to slow down depending on the effect type (Toxic, Acid).
+* */
+StatusEffect::StatusEffect()
+{
+}
+
+StatusEffect::~StatusEffect()
+{
+}
---
+++ Source/mgp25951507RF/Private/mgp25951507RFStatusEffect.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "mgp25951507RFStatusEffect.h"
+
+mgp25951507RFStatusEffect::mgp25951507RFStatusEffect()
+{
+}
+
+mgp25951507RFStatusEffect::~mgp25951507RFStatusEffect()
+{
+}
